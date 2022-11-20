5d6835e19e4e533db9d610aeab56652bc6a3866a
Helmi Pradita <helmipraditaa@gmail.com>
2022-11-20 13:04:34 +0700

    add gitignore packge-lock.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { useState } from 'react';
 import About from './pages/About';
 import Home from './pages/Home';
-import Product from './pages/Product';
-import MyProduct from './pages/MyProduct';
-import Profile from './pages/Profile';
+import DetailProduct from './pages/DetailProduct';
+import MyProduct from './pages/Dashboard/MyProduct';
+import Profile from './pages/Dashboard/Profile';
+import LoginCustomer from './pages/Auth/Login/Customer';
+import LoginSeller from './pages/Auth/Login/Seller';
+import RegisterCustomer from './pages/Auth/Register/Customer';
+import RegisterSeller from './pages/Auth/Register/Seller';
+import ResetPasswordCustomer from './pages/Auth/ResetPassword/Customer';
+import ResetPasswordSeller from './pages/Auth/ResetPassword/Seller';
+import KonfirmasiPasswordCustomer from './pages/Auth/KonfirmasiPassword/Customer';
+import KonfirmasiPasswordSeller from './pages/Auth/KonfirmasiPassword/Seller';
+import KonfirmasiPasswordLoginCustomer from './pages/Auth/KonfirmasiPasswordLogin/Customer';
+import KonfirmasiPasswordLoginSeller from './pages/Auth/KonfirmasiPasswordLogin/Seller';
+import MyBag from './pages/Dashboard/MyBag';
+import Categories from './pages/Dashboard/Categories';
 import { BrowserRouter, Route, Link, Routes, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar.js';
 
 function App() {
   const [title, setTitle] = useState();
@@ -13,13 +24,42 @@ function App() {
     <div className="App">
       <header className="App-header">{title}</header>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/myproduct" element={<MyProduct />} />
+          <Route path="/detailproduct" element={<DetailProduct />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/logincustomer" element={<LoginCustomer />} />
+          <Route path="/loginseller" element={<LoginSeller />} />
+          <Route path="/registercustomer" element={<RegisterCustomer />} />
+          <Route path="/registerseller" element={<RegisterSeller />} />
+          <Route
+            path="/resetpasswordcustomer"
+            element={<ResetPasswordCustomer />}
+          />
+          <Route
+            path="/resetpasswordseller"
+            element={<ResetPasswordSeller />}
+          />
+          <Route
+            path="/konfirmasipasswordcustomer"
+            element={<KonfirmasiPasswordCustomer />}
+          />
+          <Route
+            path="/konfirmasipasswordseller"
+            element={<KonfirmasiPasswordSeller />}
+          />
+          <Route
+            path="/konfirmasipasswordlogincustomer"
+            element={<KonfirmasiPasswordLoginCustomer />}
+          />
+          <Route
+            path="/konfirmasipasswordloginseller"
+            element={<KonfirmasiPasswordLoginSeller />}
+          />
+          <Route path="/dashboard/myproduct" element={<MyProduct />} />
+          <Route path="/dashboard/profile" element={<Profile />} />
+          <Route path="/dashboard/my-bag" element={<MyBag />} />
+          <Route path="/dashboard/categories" element={<Categories />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,66 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
+import blanja from '../../src/images/assets/blanja.png';
+import cart from '../../src/images/assets/cart.png';
+import Filter from './Filter.js';
+import Button from 'react-bootstrap/Button';
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" href="#">
-          blanja
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/home">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/product">
-                Product
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/profile">
-                Profile
-              </Link>
-            </li>
-          </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Container>
+          <Link className="navbar-brand" to="/home">
+            <img src={blanja} className="App-logo" alt="logo" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <form className="form-inline my-2 my-lg-0">
             <input
-              className="form-control mr-sm-2"
+              className="form-control mr-sm-2 rounded-pill"
               type="search"
               placeholder="Search"
               aria-label="Search"
+              style={{ width: 500 }}
             />
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Search
-            </button>
           </form>
-        </div>
+
+          <div style={{ paddingRight: 100 }}>
+            <Filter />
+          </div>
+
+          <Link className="nav-link" to="/my-bag">
+            <img src={cart} alt="cart" />
+          </Link>
+
+          <div style={{ padding: 10 }}>
+            <Button variant="danger rounded-pill" style={{ width: 100 }}>
+              <Link className="nav-link" to="/logincustomer">
+                Login
+              </Link>
+            </Button>
+          </div>
+          <div>
+            <Button
+              variant="outline-danger rounded-pill"
+              style={{ width: 100 }}
+            >
+              <Link className="nav-link" to="/signupcustomer">
+                Sign Up
+              </Link>
+            </Button>{' '}
+          </div>
+        </Container>
       </nav>
     </div>
   );
